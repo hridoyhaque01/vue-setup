312573b1d418284beafbd97571d5d7bfede352c9
Hridoy Haque <hridoyhaque.info@gmail.com>
2024-04-25 10:54:54 +0600

    fix conflict

--- src/router/index.js
+++ src/router/index.js
@@ -3,8 +3,6 @@ import Layout from "../layouts/Layout.vue";
 import AboutView from "../views/AboutView.vue";
 import Authentication from "../views/Authentication.vue";
 import HomeView from "../views/HomeView.vue";
-import About from "../pages/about/About.vue";
-import Home from "../pages/home/Home.vue";
 const routes = [
   {
     path: "/",
@@ -13,7 +11,7 @@ const routes = [
     children: [
       {
         path: "/",
-        component: Home,
+        component: HomeView,
       },
       {
         path: "/about",
@@ -33,11 +31,6 @@ const router = createRouter({
   scrollBehavior(to, from, savedPosition) {
     return { top: 0, behavior: "smooth" };
   },
-});
-
-router.beforeEach((to, from, next) => {
-  console.log(to);
-  next();
 });
 
 export default router;
